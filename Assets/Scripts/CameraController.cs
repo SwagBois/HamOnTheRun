8eb05715602071d59fde19db2090e05c4004a2f8
emestanza3 <eduardomestanza_96@gatech.edu>
2018-04-09 07:46:32 -0400

    Add files via upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = new Vector3(
+            player.transform.position.x,
+            player.transform.position.y + -3.0f,
+            player.transform.position.z + 4.0f
+        );
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float desiredAngle = player.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = player.transform.position - (rotation * offset);
+        transform.LookAt(player.transform);
+    }
+}
