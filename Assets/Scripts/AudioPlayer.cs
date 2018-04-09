8f02e1b1fcf0ae8ccc5001a2e5ce4377419ffba3
Kevin Lee <kevinlee850805@gmail.com>
2018-04-09 03:23:28 -0400

    conveyor belt has sound now

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+
+	private int triggerCount = 0;
+	private AudioSource audio;
+
+	void OnTriggerEnter( Collider collider ) {
+		AudioSource aSource = collider.gameObject.GetComponent<AudioSource>();
+
+		if( aSource != null ) {
+			aSource.Play ();
+			triggerCount++;
+		}
+	}
+
+	void OnTriggerExit( Collider collider ) {
+		AudioSource aSource = collider.gameObject.GetComponent<AudioSource>();
+
+		if( aSource != null ) {
+			triggerCount--;
+			if( triggerCount <= 0 ) {
+				aSource.Stop();
+			}
+		}
+	}
+}
