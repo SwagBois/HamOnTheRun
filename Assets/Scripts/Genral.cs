6d0aa6e37d1b7f7d95d45a5414f1963b45a1bd99
Shahwaiz Nassar <snassar3@gatech.edu>
2018-04-13 12:09:00 -0400

    Updated Pause Menu and Objective

---
+++ Assets/Scripts/Genral.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Genral : MonoBehaviour {
+
+	public GameObject Startpanel,AboutPanel,ButtonSound,PauseScreen;
+
+
+	// Use this for initialization
+	void Start () {
+		Startpanel.SetActive(true);
+		Time.timeScale = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			PauseScreen.SetActive(true);
+		}
+		
+	}
+	public void StartButtonClick()
+	{
+		Time.timeScale = 1f;
+		Startpanel.SetActive(false);
+		ButtonSound.SetActive(true);
+	}
+	public void Back()
+	{
+		AboutPanel.SetActive(false);
+		ButtonSound.SetActive(true);
+	}
+	public void About()
+	{
+		AboutPanel.SetActive(true);
+		ButtonSound.SetActive(true);
+	}
+	public void Buttonclicksound()
+	{
+		ButtonSound.SetActive(false);
+	}
+}
