6ed015c3e9dcc8a4432db356f1e95fa0b7815fb8
Kevin Lee <kevinlee850805@gmail.com>
2018-05-01 16:48:18 -0400

    crate bug fixed

---
+++ Assets/crateScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crateScript : MonoBehaviour {
+	Vector3 initialPos;
+	// Use this for initialization
+	void Start () {
+		initialPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		print("other: " + other.collider.tag);
+		print("initPos: " + initialPos);
+		if (other.collider.tag == "Magma")
+		{
+			transform.position = initialPos;
+		}
+	}
+}
